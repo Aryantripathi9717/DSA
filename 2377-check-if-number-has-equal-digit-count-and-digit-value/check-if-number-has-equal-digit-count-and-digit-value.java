8df6f86f4d6dd4263ce366c8e16277c26ea48951
Aryan Tripathi <aryantripathi639470@gmail.com>
2026-09-18 18:31:34 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (82.41%) - LeetSync

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/check-if-number-has-equal-digit-count-and-digit-value.java
@@ -1,16 +1,29 @@
 class Solution {
     public boolean digitCount(String num) {
-        Map<Character,Integer> map = new HashMap<>();
+        
+        int[] freq = new int[10];
         for(int i=0;i<num.length();i++){
-            char ch = num.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            int val = num.charAt(i)-'0';
+            freq[val]++;
         }
         for(int i=0;i<num.length();i++){
-            char key = (char)(i+'0');
-            int ch = num.charAt(i)-'0';
-            int val = map.getOrDefault(key,0);
-            if(ch!=val) return false;
+            int val = num.charAt(i)-'0';
+            if(freq[i]!=val) return false;
         }
         return true;
+
+        // have used map
+        // Map<Character,Integer> map = new HashMap<>();
+        // for(int i=0;i<num.length();i++){
+        //     char ch = num.charAt(i);
+        //     map.put(ch,map.getOrDefault(ch,0)+1);
+        // }
+        // for(int i=0;i<num.length();i++){
+        //     char key = (char)(i+'0');
+        //     int ch = num.charAt(i)-'0';
+        //     int val = map.getOrDefault(key,0);
+        //     if(ch!=val) return false;
+        // }
+        // return true;
     }
 }

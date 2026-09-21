bc2e7b95ff60f4dd0a9f2cdf67160c7b834f4f5c
Aryan Tripathi <aryantripathi639470@gmail.com>
2026-09-21 19:18:26 +0530

    Time: 11 ms (32.08%) | Memory: 47.3 MB (36.26%) - LeetSync

---
+++ 462-minimum-moves-to-equal-array-elements-ii/minimum-moves-to-equal-array-elements-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minMoves2(int[] nums) {
+        Arrays.sort(nums);
+        int median = nums[nums.length/2];
+        int ans = 0;
+        for(int i : nums){
+            ans += Math.abs(median-i);
+        }
+        return ans;
+    }
+}

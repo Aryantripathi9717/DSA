0646e49f654c42ac0b20aa35997c19e002a3654c
Aryan Tripathi <aryantripathi639470@gmail.com>
2026-09-23 23:24:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (51.52%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -1,11 +1,18 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        boolean check[] = new boolean[26];
-        for(char c : sentence.toCharArray()) check[c-'a'] = true;
-
-        for(boolean ans : check){
-            if(!ans) return false;
+        // simple method
+        for(char start = 'a'; start<='z';start++){
+            if(sentence.indexOf(start)==-1) return false;
         }
         return true;
+
+
+        // boolean check[] = new boolean[26];
+        // for(char c : sentence.toCharArray()) check[c-'a'] = true;
+
+        // for(boolean ans : check){
+        //     if(!ans) return false;
+        // }
+        // return true;
     }
 }
